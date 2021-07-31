0436bb943628ef050fb9e687413280b03e435cbc
rahulcc <rahulchawhan2@gmail.com>
2021-07-30 22:24:04 -0700

    my music website

---
+++ Music App/songs/Music App/js/music-list.js
@@ -0,0 +1,92 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Tum Saath Ho",
+      artist: "Shweta SK",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Jaan Nisaar",
+      artist: "Shweta SK",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Tere Mere",
+      artist: "Shweta SK",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Tera Chehra Jab Nazar Aaye",
+      artist: "Shweta SK",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Tere Naam",
+      artist: "Shweta SK",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Tu Jo Mila",
+      artist: "Shweta SK",
+      img: "music-6",
+      src: "music-7"
+    },
+    {
+        name: "Mere Saamne Wali Khidki",
+        artist: "Shweta SK",
+        img: "music-7",
+        src: "music-8"
+      },
+      {
+        name: "Lag Jaa Gale",
+        artist: "Shweta SK",
+        img: "music-8",
+        src: "music-6"
+      },
+      {
+        name: "Humsafar",
+        artist: "Shweta SK",
+        img: "music-9",
+        src: "music-9"
+      },
+      {
+        name: "Tujhe Kitna Chhahne",
+        artist: "Shweta SK",
+        img: "music-10",
+        src: "music-10"
+      },
+      {
+        name: "Samjhawan",
+        artist: "Shweta SK",
+        img: "music-11",
+        src: "music-11"
+      },
+      {
+        name: "Kasam Ki Kasam",
+        artist: "Shweta SK",
+        img: "music-12",
+        src: "music-12"
+      },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
